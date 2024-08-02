055c640c272831b7408327cca09dfebf6103ea39
ayaOwO <55321484+ayaOwO@users.noreply.github.com>
2024-08-02 20:59:48 +0300

    Added a while loop with 1 sec delay

--- raven-de/config-files/hypr/hyprland-config/RandomWallpaper.sh
+++ raven-de/config-files/hypr/hyprland-config/RandomWallpaper.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 WALLPAPER_DIR="/usr/share/raven-de/binaries/Wallpapers"
+while true; do 
+	sleep 1
 
-for wallpaper in $(hyprctl hyprpaper listloaded); do
-	hyprctl hyprpaper unload "$wallpaper"
+	for wallpaper in $(hyprctl hyprpaper listloaded); do
+		hyprctl hyprpaper unload "$wallpaper"
+	done
+
+	wallpaper="$(find "$WALLPAPER_DIR" -type f | shuf -n 1)"
+	hyprctl hyprpaper preload "$wallpaper"
+	hyprctl hyprpaper wallpaper ",$wallpaper"
 done
-
-wallpaper="$(find "$WALLPAPER_DIR" -type f | shuf -n 1)"
-hyprctl hyprpaper preload "$wallpaper"
-hyprctl hyprpaper wallpaper ",$wallpaper"
